e9998fb1f5daf55efb5ac346a6be1dba2d460381
Levon Ritter <levong2112@gmail.com>
2021-03-02 09:01:54 -0500

    added selenium testing project

---
+++ react-crash-selenium-tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace react_crash_selenium_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
