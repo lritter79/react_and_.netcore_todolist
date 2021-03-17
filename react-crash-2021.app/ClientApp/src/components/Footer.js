fc722e1ac48bec44ff4644fb32da2c1e20267fdd
Levon Ritter <levong2112@gmail.com>
2021-03-17 13:42:25 -0400

    resolve

--- react-crash-2021.app/ClientApp/src/components/Footer.js
+++ react-crash-2021.app/ClientApp/src/components/Footer.js
@@ -4,15 +4,9 @@ import { Link } from 'react-router-dom'
 const Footer = ({ isLoggedIn }) => {
   return (
     <footer>
-<<<<<<< HEAD
           <Link to="/" exact>Home</Link> | <Link to="/about" exact>About</Link>
           {isLoggedIn ? (<> | <Link to="/userManager" exact>Manage Account</Link> | <Link to="/alerts" exact>Alerts</Link></>) : (
                   <> | <Link to = "/login" exact>Login</Link> | <Link to="/register" exact>Register</Link></>                           
-=======
-          <Link to="/" >Home</Link> | <Link  to="/about" >About</Link>
-          {isLoggedIn ? (<></>): (
-              <> | <Link  to="/login" >Login</Link> | <Link  to="/register" >Register</Link></>                           
->>>>>>> 8d1a87fed4171d1a9d12d2b4123d310c2606f5ae
           )}
       <br></br>
       <p>Made with <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-suit-heart-fill" viewBox="0 0 16 16">
