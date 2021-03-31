ae6476b801fe0f08f986501372c0d2f094064191
Levon Ritter <levong2112@gmail.com>
2021-03-31 15:53:02 -0400

    added navbar toggle on click

--- react-crash-2021.app/ClientApp/src/components/AppNavbar.js
+++ react-crash-2021.app/ClientApp/src/components/AppNavbar.js
@@ -1,29 +1,45 @@
 ﻿import { Navbar, Nav } from 'react-bootstrap'
 import { useToken } from './api-authorization/UserContext'
 import { NavLink } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 const AppNavbar = ({ onLogoutClick, alerts }) => {
-
+    const [isExpanded, setIsExpanded] = useState(false);
     const { token } = useToken()
+
+    useEffect(() => {
+        console.log('nav bar render')
+
+        return () => {
+            console.log('nav bar clean up')
+        }
+    }, [])
+
+    useEffect(() => {
+        console.log('nav bar token use effect')
+
+        if (!token?.token) setIsExpanded(false)
+    }, [token])
+
     return (
         <>
-            <Navbar bg="light" expand="lg" sticky="top"  >
+            <Navbar bg="light" expand="lg" sticky="top" expanded={isExpanded} >
                 <Navbar.Brand>Task Tracker</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setIsExpanded(isExpanded ? false : "expanded")} />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link as={NavLink} to="/" exact>Home</Nav.Link>
-                        <Nav.Link as={NavLink} to="/about" exact>About</Nav.Link>
+                        <Nav.Link as={NavLink} onClick={() => setIsExpanded(false)} to="/" exact>Home</Nav.Link>
+                        <Nav.Link as={NavLink} onClick={() => setIsExpanded(false)} to="/about" exact>About</Nav.Link>
                         {token?.token ? (
                             <>
-                                <Nav.Link as={NavLink} to="/userManager" exact>Manage Account</Nav.Link>
-                                <Nav.Link as={NavLink} to="/alerts" exact>Alerts
-                                        {(alerts.length > 0) ? (<span id='alertCounter'>{alerts.length}</span>) : (<></>)}
+                                <Nav.Link as={NavLink} onClick={() => setIsExpanded(false)} to="/userManager" exact>Manage Account</Nav.Link>
+                                <Nav.Link as={NavLink} onClick={() => setIsExpanded(false)} to="/alerts" exact>Alerts
+                                        {(alerts.length > 0) && (<span id='alertCounter'>{alerts.length}</span>)}
                                 </Nav.Link>
-                                <Nav.Link as={NavLink} to="/logout" exact onClick={onLogoutClick}>Logout</Nav.Link>
+                                <Nav.Link as={NavLink} onClick={() => setIsExpanded(false)} to="/logout" exact onClick={onLogoutClick}>Logout</Nav.Link>
                             </>) : (<>
-                                <Nav.Link as={NavLink} to="/login" exact>Login</Nav.Link>
-                                <Nav.Link as={NavLink} to="/register" exact>Register</Nav.Link>
+                                <Nav.Link as={NavLink} onClick={() => setIsExpanded(false)} to="/login" exact>Login</Nav.Link>
+                                <Nav.Link as={NavLink} onClick={() => setIsExpanded(false)} to="/register" exact>Register</Nav.Link>
                             </>)}
                     </Nav>
                 </Navbar.Collapse>
