b7e1e66989108f94bca4ac3407d76af13640f177
Levon Ritter <levong2112@gmail.com>
2021-03-31 16:47:07 -0400

    edited about page to get rid of place holders

--- react-crash-2021.app/ClientApp/src/components/Intro.js
+++ react-crash-2021.app/ClientApp/src/components/Intro.js
@@ -6,9 +6,21 @@ const Intro = () => {
             <h4>Welcome To Task Tracker</h4>
             <ul>
                 <li>The world's most intuitive task tracker</li>
-                <li>Keep track of your most important tracks</li>
-                <li>Lorem Ipsum</li>
+                <li>Keep track of your most important things to do</li>   
+                <li>Collaborate on tasks with your friends or coworkers</li>
             </ul>
+            
+                <p>Free Version</p>
+                <ul>
+                    <li>Keep track of ten tasks at a time</li>
+                </ul>
+
+                <p>Premium Version</p>
+                <ul>
+                    <li>Keep track of an unlimited amount of tasks at a time</li>
+                    <li>Get access to the premium calendar view</li>
+                </ul>
+
         </div>
     )
 }
